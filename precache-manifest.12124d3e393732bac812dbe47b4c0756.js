51544f69c9fd2a8ec5e6b0fa3538b1dd0c9e5f28
Bobby Schaetzle <bobalu@pantearsbox.com>
2018-11-09 04:22:19 +0000

    Updates

---
+++ precache-manifest.12124d3e393732bac812dbe47b4c0756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/web/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "05773d53a80748feb222",
+    "url": "/web/static/js/runtime~main.05773d53.js"
+  },
+  {
+    "revision": "09f2de7f82ab63e336d1",
+    "url": "/web/static/js/main.09f2de7f.chunk.js"
+  },
+  {
+    "revision": "1a2fe23bbee4e72efb16",
+    "url": "/web/static/js/1.1a2fe23b.chunk.js"
+  },
+  {
+    "revision": "09f2de7f82ab63e336d1",
+    "url": "/web/static/css/main.6675fcad.chunk.css"
+  },
+  {
+    "revision": "3a78b824a7f92b3706a9e697be25b2f1",
+    "url": "/web/index.html"
+  }
+];
