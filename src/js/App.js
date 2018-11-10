40c7fa74a5fd79c348d021c62929f0d46bdb4d70
Bobby Schaetzle <bobalu@pantearsbox.com>
2018-11-10 09:35:40 +0000

    used router hookds

--- src/js/App.js
+++ src/js/App.js
@@ -1,18 +1,41 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import RouteHook from 'react-route-hook';
 import Wiki from './Wiki';
-
-const WikiPage = ({match}) => {
-  return (<Wiki page={`${match.url}`} />);
-};
+import '../scss/wiki.scss';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { 'wiki' : 'div' };
+    this.fetchWiki = this.fetchWiki.bind(this);
+    this.shouldFetchWiki = this.shouldFetchWiki.bind(this);
+  }
+
+  fetchWiki(props) {
+    import('../md' + props.match.url + '.md').then((page) => { 
+      this.setState({ 'wiki': page.default });
+    });
+  }
+
+  shouldFetchWiki(newProps, oldProps) {
+    if (oldProps.match.url !== newProps.match.url) {
+      this.fetchWiki(newProps)
+    }
+  }
+
   render() {
     return (
       <Router>
         <div>
           <Link to="/wiki/index">Wiki</Link>
-          <Route path="/wiki/**" component={WikiPage}/>
+          <Link to="/wiki/bogleg">Bogleg</Link>
+          <RouteHook
+             path="/wiki/**"
+             onEnter={this.fetchWiki} 
+             onChange={this.shouldFetchWiki}
+             render={(routerProps) => <div className="wiki"><this.state.wiki /></div>} 
+          />
         </div>
       </Router>
     );
