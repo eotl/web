f673933b97b94fe69f1b3087bd1899e0bd1657bd
Bobby Schaetzle <bobalu@pantearsbox.com>
2018-11-10 23:08:22 +0000

    buh

--- src/js/App.js
+++ src/js/App.js
@@ -25,7 +25,7 @@ class App extends Component {
 
   render() {
     return (
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <div>
           <Link to="/wiki/index">Wiki</Link>
           <Link to="/wiki/bogleg">Bogleg</Link>
