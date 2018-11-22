7beb7c4f73bcc22a90b33019838d9c699ffc4d45
Bobby Schaetzle <bobalu@pantearsbox.com>
2018-11-22 09:17:39 +0000

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/web/precache-manifest.5e79689bba727f65c08542bb4657a728.js"
+  "/web/precache-manifest.59f04ef36a46ddba4e663a6a7588222c.js"
 );
 
 workbox.clientsClaim();
