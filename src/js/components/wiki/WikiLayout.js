9ebfaa69b77186a20a9a08d26a19db5ed3403f46
Bobby Schaetzle <bobalu@pantearsbox.com>
2018-11-23 08:28:50 +0000

    buh

--- src/js/components/wiki/WikiLayout.js
+++ src/js/components/wiki/WikiLayout.js
@@ -2,23 +2,30 @@ import React, { Component } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { withStyles, MuiThemeProvider } from '@material-ui/core/styles';
 import WikiDrawer from './WikiDrawer';
+import classNames from 'classnames';
 import styles from '../../styles/layout';
 import { wiki } from '../../styles/themes';
 
 @withStyles(styles, { withTheme: true })
 class WikiLayout extends Component {
   render() {
-    const { classes, toggleAppDrawer, appDrawerOpen } = this.props;
+    const { classes, toggleWikiDrawer, wikiDrawerOpen } = this.props;
 
     return (
       <div className={classes.root}>
         <MuiThemeProvider theme={wiki}>
           <CssBaseline />
           <WikiDrawer 
-            open={appDrawerOpen} 
-            toggle={toggleAppDrawer}
+            open={wikiDrawerOpen} 
+            toggle={toggleWikiDrawer}
           />
-          {this.props.children}
+          <article
+            className={classNames(classes.content, {
+              [ classes.contentShift ] : wikiDrawerOpen
+            })}
+          >
+            {this.props.children}
+          </article>
         </MuiThemeProvider>
       </div>
     );
